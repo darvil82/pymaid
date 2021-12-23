dce1cb203848a7c573d10d27600f24f015d90c50
DarviL82 <davidlosantos89@gmail.com>
2021-12-24 00:59:19 +0100

    hohohoooo

---
+++ py-mermaid/main.py
@@ -0,0 +1,77 @@
+import argparse
+from importlib import import_module
+import sys
+from os import path
+
+import mclass
+
+DIRECTIONS = (
+	"LR",
+	"RL",
+	"TB",
+	"BT"
+)
+
+def get_import_file(file):
+	sys.path.append(path.dirname(file))
+	return import_module(path.basename(file.removesuffix(".py")))
+
+def generate_mermaid(mermaid: list[str], type: str, direction: str):
+	if (dir := direction.upper()) not in DIRECTIONS:
+		print(f"Invalid direction: {dir}")
+		quit()
+
+	return "\n".join(
+		[
+			"```mermaid",
+			type,
+			f"direction {dir}",
+			*mermaid,
+			"```"
+		]
+	)
+
+
+def do_class(args):
+	a = mclass.gen_mermaid(
+		get_import_file(args.input),
+		args.parents,
+		args.uses,
+		args.text,
+		args.props,
+		args.methods
+	)
+	print(generate_mermaid(a, "classDiagram", args.direction))
+
+
+def parse_args():
+	# temp name
+	pargs = argparse.ArgumentParser("pymegen")
+	pargs.add_argument("input", help="The input file", type=str)
+	pargs.add_argument(
+		"-d", "--direction", help=f"The direction of the diagram {DIRECTIONS}",
+		type=str, default="TB"
+	)
+	subparser = pargs.add_subparsers()
+
+	class_args = subparser.add_parser("class")
+	class_args.add_argument("-p", "--parents", action="store_false", help="Don't show the parent classes")
+	class_args.add_argument("-u", "--uses", action="store_true", help="Show the used classes")
+	class_args.add_argument("-t", "--text", action="store_true", help="Show text on the paths")
+	class_args.add_argument("-P", "--props", action="store_false", help="Don't show the properties")
+	class_args.add_argument("-m", "--methods", action="store_false", help="Don't show the methods")
+
+	class_args.set_defaults(func=do_class)
+
+	pargs.set_defaults(func=lambda _: pargs.print_help())
+
+	return pargs.parse_args()
+
+
+
+def main():
+	args = parse_args()
+	args.func(args)
+
+if __name__ == "__main__":
+	main()
--- py-mermaid/mclass.py
+++ py-mermaid/mclass.py
@@ -1,8 +1,6 @@
-from importlib import import_module
 import inspect
-import sys
-from os import path
 from typing import Callable
+
 
 
 class Class:
@@ -18,10 +16,12 @@ class Class:
 		show_path_text: bool = False,
 		show_parents: bool = True,
 		show_uses: bool = False,
+		show_props: bool = True,
+		show_methods: bool = True
 	) -> list[str]:
 		"""Returns a list of the mermaid representation of the given object"""
 		return [
-			*(gen_props_mermaid(self)),
+			*(gen_class_mermaid(self, show_props, show_methods)),
 			*(gen_uses_mermaid(self, show_path_text) if show_uses else []),
 			*(gen_parents_mermaid(self, show_path_text) if show_parents else [])
 		]
@@ -37,7 +37,6 @@ def get_function_args(func: Callable):
 	return [*inspect.signature(func).parameters]
 
 
-#! SHIT SHIT SHIT SHIT
 def get_name(obj: object) -> str:
 	"""Returns the name of the given object"""
 	return obj.__name__ if all(i in str(obj) for i in "><") else str(obj)
@@ -48,17 +47,23 @@ def get_used_objects(annotations: dict[str, object]) -> dict[str, tuple[str, str
 	Returns a dictionary of the used objects of the given object.
 	`{name: (type_name, type)}`
 	"""
-	return {k: (v.__name__, get_name(v)) for k, v in annotations.items()}
+	return {
+		k: (
+			(v if isinstance(v, str) else v.__name__),
+			get_name(v)
+		) for k, v in annotations.items()
+	}
 
 
 def gen_props_mermaid(obj: Class) -> list[str]:
 	"""Returns a list of the properties of the given object in mermaid format"""
-	content = [f"class {obj.name} {{"]
+	return [
+	    f"\t{get_name(type)}: {prop}" for prop, type in obj.annotations.items()
+	]
 
-	for prop, type in obj.annotations.items():
-		content.append(f"\t{get_name(type)}: {prop}")
 
-	# add its methods and return types
+def gen_methods_mermaid(obj: Class) -> list[str]:
+	content: list[str] = []
 	for name, method in inspect.getmembers(obj.object, predicate=inspect.isfunction):
 		try:
 			return_type: str | None = get_name(method.__annotations__["return"])
@@ -69,9 +74,21 @@ def gen_props_mermaid(obj: Class) -> list[str]:
 		content.append(
 			f"\t{name}({', '.join(get_function_args(method))}) {return_type}"
 		)
+	return content
 
-	# if no properties or methods were added, just return an empty class definition
-	return content + ["}"] if len(content) >= 3 else [f"class {obj.name}"]
+
+def gen_class_mermaid(
+	obj: Class,
+	show_props: bool = True,
+	show_methods: bool = True
+) -> list[str]:
+	content = [
+		f"class {obj.name} {{",
+		*(gen_props_mermaid(obj) if show_props else []),
+		*(gen_methods_mermaid(obj) if show_methods else []),
+		"}"
+	]
+	return content if len(content) >= 3 else [f"class {obj.name}"]
 
 
 def gen_uses_mermaid(obj: Class, display_use_path: bool = True) -> list[str]:
@@ -83,7 +100,7 @@ def gen_uses_mermaid(obj: Class, display_use_path: bool = True) -> list[str]:
 		if type[0] in shown:
 			continue
 		count = tuple(x[0] for x in obj.uses.values()).count(type[0])
-		new.append(f'{obj.name} "{count}" ..> {type[0]}: {type[1] if display_use_path else ""}')
+		new.append(f'{obj.name} "{count}" ..> {type[0]}{f": {type[1]}" * display_use_path}')
 		shown.append(type[0])
 
 	return new
@@ -92,35 +109,26 @@ def gen_uses_mermaid(obj: Class, display_use_path: bool = True) -> list[str]:
 def gen_parents_mermaid(obj: Class, display_parent_path: bool = True) -> list[str]:
 	"""Returns a list of the parents of the given object in mermaid format"""
 	return [
-		f"{parent.__name__} <|-- {obj.name}: {'parent' * display_parent_path}"
+		f"{parent.__name__} <|-- {obj.name}{': parent' * display_parent_path}"
 		for parent in obj.parents
 	]
 
 
 def gen_mermaid(
 	objects: list[Class],
-	show_path_text: bool = False,
 	show_parents: bool = True,
 	show_uses: bool = False,
+	show_path_text: bool = False,
+	show_properties: bool = True,
+	show_methods: bool = True
 ) -> list[str]:
 	"""Returns a list of the mermaid representation of the given objects"""
-	content: list[str] = ["```mermaid", "classDiagram", "direction LR"]
+	content: list[str] = []
 
-	for obj in objects:
+	for obj in get_classes(objects):
 		# add class mermaid representation
-		content += [""] + obj.get_mermaid(show_path_text, show_parents, show_uses)
+		content += [""] + obj.get_mermaid(
+			show_path_text, show_parents, show_uses, show_properties, show_methods
+		)
 
-	return content + ["```"]
-
-
-def main():
-	sys.path.append(path.dirname(sys.argv[1]))
-	mod = import_module(path.basename(sys.argv[1].removesuffix(".py")))
-	members = get_classes(mod)
-
-	with open("test.md", "w") as f:
-		f.write("\n".join(gen_mermaid(members, True, True, True)))
-
-
-if __name__ == "__main__":
-	main()
+	return content
