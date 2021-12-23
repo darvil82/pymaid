bb0a6b34068942a39bfb286ff96c21474dc4d5f1
trag1c <77130613+trag1c@users.noreply.github.com>
2021-12-23 16:59:37 +0100

    :]

--- class.py
+++ class.py
@@ -1,73 +1,8 @@
-from importlib import import_module, util
+from importlib import import_module
 import inspect
 import sys
 from os import path
-from typing import Any
-
-
-def get_classes(obj) -> list["Class"]:
-	"""Returns a list of all classes in the given object"""
-	return [Class(obj) for name, obj in inspect.getmembers(obj, predicate=inspect.isclass)]
-
-def get_function_args(func):
-	"""Returns a list of the arguments of a function"""
-	return list(inspect.signature(func).parameters)
-
-#! SHIT SHIT SHIT SHIT
-def get_name(obj: Any, basic_name: bool = True) -> str:
-	"""Returns the name of the given object"""
-	return obj.__name__ if "<" in str(obj) and ">" in str(obj) else str(obj)
-
-def get_used_objects(annotations: dict[str, Any]) -> dict[str, tuple[str, str]]:
-	"""
-	Returns a dictionary of the used objects of the given object.
-	`{name: (type_name, type)}`
-	"""
-	return {k: (v.__name__, get_name(v)) for k, v in annotations.items()}
-
-def gen_props_mermaid(obj: "Class") -> list[str]:
-	"""Returns a list of the properties of the given object in mermaid format"""
-	content = [f"class {obj.name} {{"]
-
-	for prop, type in obj.annotations.items():
-		content.append(f"\t{get_name(type)}: {prop}")
-
-	# add its methods and return types
-	for name, method in inspect.getmembers(obj.object, predicate=inspect.isfunction):
-		try:
-			return_type = get_name(method.__annotations__["return"])
-		except (AttributeError, KeyError):
-			return_type = None
-
-		# the method representation
-		content.append(
-			f"\t{name}({', '.join(get_function_args(method))}) {return_type}"
-		)
-
-	# if no properties or methods were added, just return an empty class definition
-	return (content + ["}"] if len(content) >= 3 else [f"class {obj.name}"])
-
-def gen_uses_mermaid(obj: "Class", display_use_path: bool = True) -> list[str]:
-	"""Returns a list of the used objects of the given object in mermaid format"""
-	new = []	# the new mermaid representation
-	shown = []	# used to prevent duplicates
-
-	for name, type in obj.uses.items():
-		if type[0] in shown:
-			continue
-		count = tuple(x[0] for x in obj.uses.values()).count(type[0])
-		new.append(f'{obj.name} "{count}" ..> {type[0]}: {type[1] if display_use_path else ""}')
-		shown.append(type[0])
-
-	return new
-
-def gen_parents_mermaid(obj: "Class", display_parent_path: bool = True) -> list[str]:
-	"""Returns a list of the parents of the given object in mermaid format"""
-	return [
-		f"{parent.__name__} <|-- {obj.name}: {'parent' if display_parent_path else ''}"
-		for parent in obj.parents
-	]
-
+from typing import Callable
 
 
 class Class:
@@ -86,14 +21,80 @@ class Class:
 	) -> list[str]:
 		"""Returns a list of the mermaid representation of the given object"""
 		return [
-			*gen_props_mermaid(self),
+			*(gen_props_mermaid(self)),
 			*(gen_uses_mermaid(self, show_path_text) if show_uses else []),
 			*(gen_parents_mermaid(self, show_path_text) if show_parents else [])
 		]
 
 
+def get_classes(obj: object) -> list[Class]:
+	"""Returns a list of all classes in the given object"""
+	return [Class(obj) for _, obj in inspect.getmembers(obj, predicate=inspect.isclass)]
 
 
+def get_function_args(func: Callable):
+	"""Returns a list of the arguments of a function"""
+	return [*inspect.signature(func).parameters]
+
+
+#! SHIT SHIT SHIT SHIT
+def get_name(obj: object) -> str:
+	"""Returns the name of the given object"""
+	return obj.__name__ if all(i in str(obj) for i in "><") else str(obj)
+
+
+def get_used_objects(annotations: dict[str, object]) -> dict[str, tuple[str, str]]:
+	"""
+	Returns a dictionary of the used objects of the given object.
+	`{name: (type_name, type)}`
+	"""
+	return {k: (v.__name__, get_name(v)) for k, v in annotations.items()}
+
+
+def gen_props_mermaid(obj: Class) -> list[str]:
+	"""Returns a list of the properties of the given object in mermaid format"""
+	content = [f"class {obj.name} {{"]
+
+	for prop, type in obj.annotations.items():
+		content.append(f"\t{get_name(type)}: {prop}")
+
+	# add its methods and return types
+	for name, method in inspect.getmembers(obj.object, predicate=inspect.isfunction):
+		try:
+			return_type: str | None = get_name(method.__annotations__["return"])
+		except (AttributeError, KeyError):
+			return_type = None
+
+		# the method representation
+		content.append(
+			f"\t{name}({', '.join(get_function_args(method))}) {return_type}"
+		)
+
+	# if no properties or methods were added, just return an empty class definition
+	return content + ["}"] if len(content) >= 3 else [f"class {obj.name}"]
+
+
+def gen_uses_mermaid(obj: Class, display_use_path: bool = True) -> list[str]:
+	"""Returns a list of the used objects of the given object in mermaid format"""
+	new = []	# the new mermaid representation
+	shown = []	# used to prevent duplicates
+
+	for _, type in obj.uses.items():
+		if type[0] in shown:
+			continue
+		count = tuple(x[0] for x in obj.uses.values()).count(type[0])
+		new.append(f'{obj.name} "{count}" ..> {type[0]}: {type[1] if display_use_path else ""}')
+		shown.append(type[0])
+
+	return new
+
+
+def gen_parents_mermaid(obj: Class, display_parent_path: bool = True) -> list[str]:
+	"""Returns a list of the parents of the given object in mermaid format"""
+	return [
+		f"{parent.__name__} <|-- {obj.name}: {'parent' * display_parent_path}"
+		for parent in obj.parents
+	]
 
 
 def gen_mermaid(
@@ -110,7 +111,6 @@ def gen_mermaid(
 		content += [""] + obj.get_mermaid(show_path_text, show_parents, show_uses)
 
 	return content + ["```"]
-
 
 
 def main():
